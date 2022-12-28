a014944a12efc62b490d2a3f5b1aa346b63ffc35
pray3m <prem.gtm9@gmail.com>
2022-12-28 20:29:45 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FormInput from './components/FormInput'
+
+function App() {
+
+  return (
+    <div className="app">
+      <FormInput/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FormInput = () => {
+  return (
+    <div>
+        <label>Username</label>
+        <input type="text" name="" id="" />
+    </div>
+  )
+}
+
+export default FormInput
