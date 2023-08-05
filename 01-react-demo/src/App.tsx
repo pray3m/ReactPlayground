9b2307f32ce18c204c894f1e9390f2df393146a1
pray3m <prem.gtm9@gmail.com>
2023-08-06 00:17:30 +0545

    add : 01 react demo

---
+++ 01-react-demo/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ExpenseTracker from "./expense-tracker/ExpenseTracker";
+
+function App() {
+  return (
+    <div>
+      <ExpenseTracker />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-react-demo/src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,23 @@
+import categories from "../categories";
+
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(e) => onSelectCategory(e.target.value)}
+    >
+      <option value="">All categories</option>
+      {categories.map((category) => (
+        <option value={category} key={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default ExpenseFilter;
