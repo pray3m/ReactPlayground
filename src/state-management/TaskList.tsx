7c3cb47506aad6e9aab6fc9e6d607fa9ca9260e8
pray3m <prem.gtm9@gmail.com>
2024-06-17 00:10:19 +0545

    Exercise - creating a custom provider and hook for tasks

--- src/state-management/TaskList.tsx
+++ src/state-management/TaskList.tsx
@@ -1,12 +1,11 @@
-import { useContext } from "react";
-import TasksContext from "./contexts/tasksContext";
-import AuthContext from "./contexts/authContext";
+import useAuth from "./hooks/useAuth";
+import useTasks from "./hooks/useTasks";
 
 const TaskList = () => {
   // const [tasks, setTasks] = useState<Task[]>([]);
 
-  const { tasks, dispatch } = useContext(TasksContext);
-  const { user } = useContext(AuthContext);
+  const { tasks, dispatch } = useTasks();
+  const { user } = useAuth();
 
   return (
     <>
