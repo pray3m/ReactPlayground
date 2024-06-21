84f4a41e77324942bdc039262e8c35a8430caefc
pray3m <prem.gtm9@gmail.com>
2024-06-21 10:50:58 +0545

    Optimizing re-renders using selectors

--- src/zustand/NavBar.tsx
+++ src/zustand/NavBar.tsx
@@ -2,7 +2,9 @@ import LoginStatus from "../zustand/auth/LoginStatus";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
+
+  console.log("Navbar render");
 
   return (
     <nav className="navbar d-flex justify-content-between">
